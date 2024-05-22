dc1b222b1ddaf01682287bf1e9f417d8856a87b7
Rafay Ahmed <142908388+Rafay917@users.noreply.github.com>
2024-05-22 20:12:17 +0500

    Create Bank_Pin_Code.py

---
+++ Bank_Pin_Code.py
@@ -0,0 +1,10 @@
+def validate_pin(pin):
+  if ( len(pin)==4 or len(pin) == 6) and pin.isdigit():
+    return True
+  else:
+    return False
+print(validate_pin(input("Enter the Bank Pin Code : ")))
+print(validate_pin(input("Enter the Bank Pin Code : ")))
+print(validate_pin(input("Enter the Bank Pin Code : ")))
+print(validate_pin(input("Enter the Bank Pin Code : ")))
+print(validate_pin(input("Enter the Bank Pin Code : ")))
