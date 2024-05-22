5fe5c0970a034c0c4f560f9841405c07e4af896a
Rafay Ahmed <142908388+Rafay917@users.noreply.github.com>
2024-05-22 20:03:54 +0500

    Update Bank-Pin-Code.py

--- Bank-Pin-Code.py
+++ Bank-Pin-Code.py
@@ -1,8 +1,11 @@
 # Bank Pin Code In Python
 def validate_pin(pin):
-  if ( len(pin)==4 or len(pin) == 6) and pin.isdigit()
-print(validate_pin(input("Enter the Bank Pin Code"))
-print(validate_pin(input("Enter the Bank Pin Code"))
-print(validate_pin(input("Enter the Bank Pin Code"))
-print(validate_pin(input("Enter the Bank Pin Code"))
-print(validate_pin(input("Enter the Bank Pin Code"))
+  if ( len(pin)==4 or len(pin) == 6) and pin.isdigit():
+    return True
+  else:
+    return False
+print(validate_pin(input("Enter the Bank Pin Code : ")))
+print(validate_pin(input("Enter the Bank Pin Code : ")))
+print(validate_pin(input("Enter the Bank Pin Code : ")))
+print(validate_pin(input("Enter the Bank Pin Code : ")))
+print(validate_pin(input("Enter the Bank Pin Code : ")))
